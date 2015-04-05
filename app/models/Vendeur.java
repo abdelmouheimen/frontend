10829a9cd8d1493339b33ce57a32a03d449ec558
nairouz <nairouz.mrabah@gmail.com>
2015-04-05 02:19:43 +0200

    models "correction"

---
+++ app/models/Vendeur.java
@@ -0,0 +1,24 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Created by Dell on 05/04/2015.
+ */
+@Entity
+@Inheritance(strategy= InheritanceType.JOINED)
+public class Vendeur extends Model {
+    @Id
+    public Long id;
+    public String nom;
+    public String prenom;
+    public String login;
+    public Double salaire;
+    public String passwd;
+    public Long   tel;
+    @OneToMany
+    public List<Objectifs> objectifs;
+}
